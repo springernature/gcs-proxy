a6434315c8c7562e0438fc9892905b6a4e2b783c
Simon Johansson <simon@simonjohansson.com>
2018-10-31 11:39:26 +0100

    Empty project

---
+++ go.mod
@@ -0,0 +1,1 @@
+module github.com/springernature/gcs-proxy
---
+++ server_test.go
@@ -0,0 +1,10 @@
+package gcs_proxy
+
+import (
+	"testing"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestItRespondsWithA200OnSlash(t *testing.T) {
+	assert.True(t, true)
+}
